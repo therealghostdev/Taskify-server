68c264edb8f548d07581b6b1f88821b097db003a
DaScOrPiO <dunni82@yahoo.com>
2024-05-07 13:11:05 +0100

    eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,17 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import { browser } from "globals";
+import eslintRecommended from "@eslint/eslint-plugin/dist/configs/recommended";
+import typescriptEslintRecommended from "typescript-eslint/dist/recommended";
 
-
-export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-];
+export default {
+  languageOptions: { globals: { browser } },
+  extends: [eslintRecommended, "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    // Additional rules can be added here
+  },
+};
