5cf2fe956dafb576a9955ed598cd5f0a280e23f8
DaScOrPiO <dunni82@yahoo.com>
2024-05-07 14:29:01 +0100

    eslint & workflow updates

---
+++ .github/eslint-config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    root: true,
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint"],
+    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+    rules: {
+      // Add your custom rules here
+    },
+    env: {
+      node: true,
+      es6: true,
+    },
+    globals: {
+      console: "readonly",
+    },
+    overrides: [
+      {
+        files: ["*.ts", "*.tsx"],
+        parserOptions: {
+          project: "./tsconfig.json",
+        },
+      },
+      {
+        files: ["dist/**/*.js"],
+        rules: {
+          "@typescript-eslint/no-var-requires": "off",
+        },
+      },
+    ],
+  };
+  
