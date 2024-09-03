79fb41d0887077d1a1487f67e558ebac20809ba5
DaScOrPiO <dunni82@yahoo.com>
2024-09-03 18:00:50 +0100

    FIX: rate limiter

--- dist/routes/googleAuth.js
+++ dist/routes/googleAuth.js
@@ -7,13 +7,14 @@ exports.googleAuthRouter = void 0;
 const passport_1 = __importDefault(require("passport"));
 const express_1 = __importDefault(require("express"));
 const login_register_1 = require("../utils/middlewares/routes/login&register");
+const rate_limiter_1 = require("../config/rate-limiter");
 exports.googleAuthRouter = express_1.default.Router();
 // initilize google authentication
-exports.googleAuthRouter.get("/google_auth", (req, res, next) => {
+exports.googleAuthRouter.get("/google_auth", rate_limiter_1.limiter, (req, res, next) => {
     const { username } = req.query;
     passport_1.default.authenticate("google", {
         scope: ["profile", "email"],
         state: username,
     })(req, res, next);
 });
-exports.googleAuthRouter.get("/google/callback", passport_1.default.authenticate("google", { failureRedirect: "/" }), login_register_1.googleAuth);
+exports.googleAuthRouter.get("/google/callback", rate_limiter_1.limiter, passport_1.default.authenticate("google", { failureRedirect: "/" }), login_register_1.googleAuth);
--- index.ts
+++ index.ts
@@ -15,7 +15,6 @@ import { startRedis } from "./config/redis/client";
 import mongoSanitize from "express-mongo-sanitize";
 import cookieParser from "cookie-parser";
 import { userRoute } from "./routes/user";
-import rateLimit from "express-rate-limit";
 
 const envFile =
   process.env.NODE_ENV === "production"
@@ -73,15 +72,6 @@ app.use(
     credentials: true,
   })
 );
-
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  limit: 200,
-  standardHeaders: "draft-7",
-  legacyHeaders: false,
-});
-
-app.use(limiter);
 
 app.use("/user", userRoute);
 app.use("/taskify/v1/auth", appleAuthRouter);
